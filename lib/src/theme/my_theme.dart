a15acdf999575988d3ed4d871ac61fceb672222d
Tyrone Pesantes <pesantestyrone@gmail.com>
2020-10-18 00:46:46 -0500

    agregar secondary color al theme

--- lib/src/theme/my_theme.dart
+++ lib/src/theme/my_theme.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 final myTheme = ThemeData.dark().copyWith(
   accentColor: Colors.red,
   //primaryColor: Colors.yellow,
+  secondaryHeaderColor: Colors.blue,
 );
